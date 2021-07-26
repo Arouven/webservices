d15207480e28d6a1430f943bd874d003f05b7acc
Arouven <aroupoolian@gmail.com>
2021-07-26 14:39:54 +0400

    reorder

--- 2/apis/apianalyser.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'allCard.php';
-
-
-// Use parse_str() function to parse the
-// string passed via URL
-parse_str($_SERVER["QUERY_STRING"], $params); // store all queries in $params
